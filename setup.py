8f1d7752d8874094e4f52d5b0dda60c92ee8a9f3
Keshav Varma <keshav@yelp.com>
2015-08-12 19:28:50 -0700

    Update setup.py to include github repo

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "chronos-python",
@@ -20,4 +18,5 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
     ],
+    url='https://github.com/asher/chronos-python',
 )
